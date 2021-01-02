558a0313fa6bfc3e1e987dcb70f08775f11bb86b
Kazuki Imamura <kaizumaki@gmail.com>
2021-01-02 20:29:45 +0900

    vue-loader, vue-svg-loaderをインストール・vue.config.jsにsvgの読み込み設定を追加

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,15 @@ module.exports = {
         prependData: `@import "~@/assets/variables.scss";`
       }
     }
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
   }
 }
