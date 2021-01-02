08ea4fb57ffeffd983c27b3b1430d5a26e4ec450
Kazuki Imamura <kaizumaki@gmail.com>
2021-01-02 23:47:31 +0900

    Appからnavを削除

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -13,18 +9,5 @@
   max-width: 600px;
   margin: 0 auto;
   padding: 16px;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
