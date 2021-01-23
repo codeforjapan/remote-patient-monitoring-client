538d5fd30830cd53d46ec1b0aa874424a0936d9d
hiroaki terayama <hiro.nnsa724@gmail.com>
2021-01-23 16:01:14 +0900

    Messageコンポーネント実装

---
+++ src/components/Message.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="message" :class="`type${messageType}`">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    isError: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    messageType() {
+      return this.isError ? 'Error' : 'Normal'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.message {
+  border-radius: 10px;
+  margin: 16px 0;
+  padding: 16px;
+}
+.typeNormal {
+  background-color: $alert-normal;
+}
+.typeError {
+  background-color: $alert-error;
+  font-weight: bold;
+}
+</style>
