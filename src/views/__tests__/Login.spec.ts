ade3751a611a989f29760a971d2ea7eb4a908084
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-02-21 17:27:00 +0900

    add first test case

---
+++ src/views/__tests__/Login.spec.ts
@@ -0,0 +1,40 @@
+// src/components/__tests__/Login.spec.ts
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Login from '../Login.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueRouter)
+
+describe('Login.vue', () => {
+  let store: any
+  let Auth
+
+  beforeEach(() => {
+    Auth = {
+      namespaced: true,
+      getters: {
+        isLoggedIn: () => false,
+      },
+      actions: {},
+    }
+
+    store = new Vuex.Store({
+      modules: {
+        Auth,
+      },
+    })
+  })
+
+  test('renders props.msg when passed', () => {
+    const wrapper = shallowMount(Login, {
+      store,
+      localVue,
+      stubs: ['router-link'],
+    })
+    expect(wrapper.text()).toMatch('ログイン')
+  })
+  test.todo('handle login and go to render view afeter ')
+})
