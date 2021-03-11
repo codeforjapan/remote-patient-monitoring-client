42a809fc3c74ccfd818de8aa1185a94eb7e1ed02
halsk <halsk@users.noreply.github.com>
2021-03-11 03:51:55 +0000

    deploy: 009748c1701e2b547e10870057258961ee370ca0

---
+++ precache-manifest.233f643c92c1fccf3d6bb3fbd1920ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b4d854306f732416d2",
+    "url": "/css/app.830f631c.css"
+  },
+  {
+    "revision": "bc0f1a9506ff8cd02cfa64378fa4f73b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b4d854306f732416d2",
+    "url": "/js/app.271e1643.js"
+  },
+  {
+    "revision": "a148dd72994f9a40f392",
+    "url": "/js/chunk-vendors.d5984f5d.js"
+  },
+  {
+    "revision": "3627e2b39e8236f577c7501bed603e4f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
